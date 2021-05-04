04889ca6a24014610dfa807d2d45e8a1c5f8d546
John Ulbright <john.ulbright@gmail.com>
2021-05-04 11:14:12 -0400

    starts prepping for deployment

--- db.js
+++ db.js
@@ -1,5 +1,7 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_URL)
+const sequelize = new Sequelize(process.env.DB_URL,{
+    dialect:'postgres',
+})
 
 module.exports = sequelize;
