1df3bfaf29e950555d00ffe22211c3cc202617fd
johnulbright <john.ulbright@gmail.com>
2021-05-04 20:46:31 -0400

    changes db to database

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_URL,{
+const sequelize = new Sequelize(process.env.DATABASE_URL,{
     dialect:'postgres',
 })
 
