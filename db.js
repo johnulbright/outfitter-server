40c10a8d5035249b354341d2d6c63766d9256ff4
johnulbright <john.ulbright@gmail.com>
2021-05-04 20:55:19 -0400

    db.js tweak

--- db.js
+++ db.js
@@ -1,7 +1,13 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL,{
-    dialect:'postgres',
-})
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: !process.env.DATABASE_URL.includes('localhost') ? {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // <<<<<<< YOU NEED THIS TO FIX UNHANDLED REJECTION 
+      },
+    } : {}
+  });
 
 module.exports = sequelize;
